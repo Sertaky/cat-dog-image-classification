5796443c58e37d337af0aa0654334b5bef480e23
A.R <sertaky_25@yahoo.com>
2026-09-21 14:42:38 +0300

    Add BatchNorm GAP CNN experiment

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,5 +2,6 @@
 
 from .simple_cnn import SimpleCNN
 from .simple_cnn_gap import SimpleCNNGAP
+from .simple_cnn_gap_bn import SimpleCNNGAPBatchNorm
 
-__all__ = ["SimpleCNN", "SimpleCNNGAP"]
+__all__ = ["SimpleCNN", "SimpleCNNGAP", "SimpleCNNGAPBatchNorm"]
